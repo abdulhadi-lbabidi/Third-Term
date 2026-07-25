4ff55ad2d9eb9a5fa5ddc84e87a4c0fdb710c35b
Joudy Jreis <joudyjries7@gmail.com>
2026-07-25 16:41:10 +0300

    some edit

--- src/features/funds/components/attach-currency.dialog.tsx
+++ src/features/funds/components/attach-currency.dialog.tsx
@@ -13,7 +13,6 @@ import {
 } from '@/shared/components/ui/form';
 import { Input } from '@/shared/components/ui/input';
 import type { Currency } from '@/features/currencies/types';
-import { attachFundCurrencySchema, type AttachFundCurrencyValues } from '../schemas/funds.schema';
 
 type AttachCurrencyDialogProps = {
   open: boolean;
